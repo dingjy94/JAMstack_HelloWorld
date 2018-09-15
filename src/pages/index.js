747e009b82cf552ea367e2a5e3c8bf254ce49c4a
JINGYIDING <dingjy94@gmail.com>
2018-09-15 00:26:46 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello everyone</h1>
+    <p>I'm Jingyi Ding</p>
+    <p>A new graduated CS student and a font-end developer.</p>
+    <p>Hope to make more friends and do some cool things together!</p>
+    <a href="http://jingyid.me/Protfolio/" target="_blank">My poor Protfolio</a>
+  </div>
+)
+
+export default IndexPage
